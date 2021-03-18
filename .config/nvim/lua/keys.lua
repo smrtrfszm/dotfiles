a1c9eba2a95d0eed5d48ed5e29d333eac6c2c87b
Szepesi Tibor <sztibi023@gmail.com>
2021-03-18 21:08:55 +0100

    move tab keymaps to keys.lua

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -21,9 +21,14 @@ key('i', '<Left>', '<nop>')
 key('v', '<Tab>', '>gv')
 key('v', '<S-Tab>', '<gv')
 
+-- Smart tabs
+vim.api.nvim_set_keymap('i', '<TAB>', 'pumvisible() ? "<C-n>" : "<TAB>"', { expr = true, noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<S-TAB>', 'pumvisible() ? "<C-p>" : "<S-TAB>"', { expr = true, noremap = true, silent = true })
+
 key('n', '<Leader><Leader>', '<C-^>')
 key('i', '<C-j>', '<esc>')
 key('n', '<Leader>w', '<cmd>w<cr>')
 key('n', '<C-f>', '<cmd>Telescope git_files<cr>')
 key('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
 key('n', '<Leader>g', '<cmd>LazyGit<cr>')
+
--- .config/nvim/lua/plugins/nvim-compe.lua
+++ .config/nvim/lua/plugins/nvim-compe.lua
@@ -25,28 +25,3 @@ require('compe').setup({
     treesitter = true;
   }
 })
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t '<C-n>'
-  else
-    return t '<Tab>'
-  end
-end
-
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t '<C-p>'
-  else
-    return t '<S-Tab>'
-  end
-end
-
-vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
