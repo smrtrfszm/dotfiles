1ef72be06cfa304c61f2d91a5f9a674d2f1f8e4b
Szepesi Tibor <sztibi023@gmail.com>
2021-03-18 23:10:51 +0100

    change pumheight

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -3,6 +3,7 @@ vim.api.nvim_set_option('scrolloff', 5)
 vim.api.nvim_set_option('laststatus', 2)
 vim.api.nvim_set_option('showmode', false)
 vim.api.nvim_set_option('termguicolors', true)
+vim.api.nvim_set_option('pumheight', 10)
 
 vim.api.nvim_win_set_option(0, 'number', true)
 vim.api.nvim_win_set_option(0, 'relativenumber', true)
