6d58fb39a06c7a82d288c6c4408d38222dec4872
Szepesi Tibor <sztibi023@gmail.com>
2021-03-18 21:02:11 +0100

    set custom lsp icons

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,29 @@
 local lsp = require('lspconfig')
 
+vim.fn.sign_define('LspDiagnosticsSignError', {
+  texthl = 'LspDiagnosticsSignError',
+  numhl = 'LspDiagnosticsSignError',
+  text = '',
+})
+
+vim.fn.sign_define('LspDiagnosticsSignWarning', {
+  texthl = 'LspDiagnosticsSignWarning',
+  numhl = 'LspDiagnosticsSignWarning',
+  text = '',
+})
+
+vim.fn.sign_define('LspDiagnosticsSignInformation', {
+  texthl = 'LspDiagnosticsSignInformation',
+  numhl = 'LspDiagnosticsSignInformation',
+  text = '',
+})
+
+vim.fn.sign_define('LspDiagnosticsSignHint', {
+  texthl = 'LspDiagnosticsSignHint',
+  numhl = 'LspDiagnosticsSignHint',
+  text = '',
+})
+
 local servers = {
   'rust_analyzer',
   'tsserver', 
