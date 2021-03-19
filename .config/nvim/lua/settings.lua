6eddd912fde880d113accb2e7b4d5b639530ee0c
Szepesi Tibor <sztibi023@gmail.com>
2021-03-19 19:34:58 +0100

    move autocmds to lua

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -14,3 +14,17 @@ vim.api.nvim_win_set_option(0, 'signcolumn', 'yes')
 vim.api.nvim_buf_set_option(0, 'undofile', true)
 
 vim.cmd('syntax on')
+
+
+vim.cmd('autocmd Filetype javascript setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype typescript setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype html setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype css setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype scss setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype haskell setlocal tabstop=4 shiftwidth=4 expandtab softtabstop')
+vim.cmd('autocmd Filetype java setlocal colorcolumn=121')
+vim.cmd('autocmd Filetype c setlocal tabstop=4 shiftwidth=4 expandtab softtabstop colorcolumn=81')
+vim.cmd('autocmd Filetype cpp setlocal tabstop=4 shiftwidth=4 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype rust setlocal tabstop=4 shiftwidth=4 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype yaml setlocal tabstop=2 shiftwidth=2 expandtab softtabstop')
+vim.cmd('autocmd Filetype lua setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
