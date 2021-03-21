c8cf33f689c7c5309fc502ed6600532f311c0a6f
Szepesi Tibor <sztibi023@gmail.com>
2021-03-21 23:37:33 +0100

    add react autocmd

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -18,6 +18,7 @@ vim.cmd('syntax on')
 
 vim.cmd('autocmd Filetype javascript setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
 vim.cmd('autocmd Filetype typescript setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
+vim.cmd('autocmd Filetype typescriptreact setlocal tabstop=3 shiftwidth=2 expandtab softtabstop colorcolumn=121')
 vim.cmd('autocmd Filetype html setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
 vim.cmd('autocmd Filetype css setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
 vim.cmd('autocmd Filetype scss setlocal tabstop=2 shiftwidth=2 expandtab softtabstop colorcolumn=121')
