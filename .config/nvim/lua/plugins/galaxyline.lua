6d1f1296415879146d5de6b085b1c589b8310a26
Szepesi Tibor <sztibi023@gmail.com>
2021-04-21 00:02:16 +0200

    modify galaxyline config

--- .config/nvim/lua/plugins/galaxyline.lua
+++ .config/nvim/lua/plugins/galaxyline.lua
@@ -1,3 +1,4 @@
+local vim = _G.vim
 local gls = require('galaxyline').section
 local condition = require('galaxyline.condition')
 local colors = require('colors')
@@ -5,13 +6,19 @@ local colors = require('colors')
 local bg_color = colors.gray1
 
 local modes = {
-  n = {'NORMAL', colors.green},
-  i = {'INSERT', colors.blue},
-  c = {'COMMAND', colors.cyan},
-  v = {'VISUAL', colors.orange},
-  V = {'VISUAL LINE', colors.orange},
-  [''] = {'VISUAL BLOCK', colors.orange},
-  t = {'TERMINAL', colors.yellow},
+  n =      {'NORMAL', colors.green},
+  i =      {'INSERT', colors.blue},
+  c =      {'COMMND', colors.magenta},
+  v =      {'VISUAL', colors.orange},
+  V =      {'V LINE', colors.orange},
+  [''] = {'V BLCK', colors.orange},
+  t =      {'TERMNL', colors.yellow},
+}
+
+local fileformats = {
+  dos  = 'CRLF',
+  unix = 'LF',
+  mac  = 'CR',
 }
 
 gls.left = {
@@ -36,38 +43,65 @@ gls.mid = {
 }
 
 gls.right = {
-  {FileEncode = {
-    provider = 'FileEncode',
-    condition = condition.hide_in_width,
-    separator = ' ',
+  {FileType = {
+    provider = function()
+      return vim.bo.filetype
+    end,
+    separator = '  ',
     separator_highlight = {'none', bg_color},
-    highlight = {colors.green, bg_color, 'bold'}
+    highlight = {colors.white, bg_color, 'bold'}
   }},
   {FileFormat = {
-    provider = 'FileFormat',
+    provider = function()
+      return fileformats[vim.bo.fileformat]
+    end,
+    condition = condition.hide_in_width,
+    separator = '  ',
+    separator_highlight = {'none', bg_color},
+    highlight = {colors.white, bg_color }
+  }},
+  {FileEncode = {
+    provider = function()
+      local encode = vim.bo.fenc ~= '' and vim.bo.fenc or vim.o.enc
+      return encode:upper()
+    end,
     condition = condition.hide_in_width,
     separator = ' ',
     separator_highlight = {'none', bg_color},
-    highlight = {colors.green, bg_color, 'bold'}
+    highlight = {colors.white, bg_color}
   }},
   {GitBranch = {
     provider = 'GitBranch',
     icon = ' ',
     condition = condition.check_git_workspace,
-    separator = ' ',
+    separator = '  ',
     separator_highlight = {'none', bg_color},
     highlight = {colors.magenta, bg_color}
   }},
   {LineInfo = {
-    provider = 'LineColumn',
+    provider = function()
+      local col = vim.fn.col('.')
+      local line = vim.fn.line('.')
+
+      local info = '  ' .. line .. ':' .. col
+
+      local curr = vim.fn.line('.')
+      local total = vim.fn.line('$')
+      local percent = math.floor(curr * 100 / total) .. '%'
+
+      local mode = modes[vim.fn.mode()]
+      vim.cmd('hi GalaxyLineInfo guibg=' .. mode[2])
+
+      if curr == 1 then
+        percent = 'Top'
+      elseif curr == total then
+        percent = 'Bottom'
+      end
+
+      return info .. ' | ' .. percent .. ' '
+    end,
     separator = ' ',
     separator_highlight = {'none', bg_color},
-    highlight = {colors.gray6, bg_color}
-  }},
-  {Percent = {
-    provider = 'LinePercent',
-    separator = ' ',
-    separator_highlight = {'none', bg_color},
-    highlight = {bg_color, colors.blue, 'bold'},
+    highlight = {bg_color, colors.red},
   }},
 }
