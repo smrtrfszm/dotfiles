6607ab4809ba37aa47c684b5da4e9529e6c7d7fe
Szepesi Tibor <sztibi023@gmail.com>
2021-04-24 17:29:37 +0200

    simplify mouse bindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -188,19 +188,8 @@ toggleWindows = do
 
 -- Mouse bindings
 myMouseBindings (XMonad.XConfig {modMask = modm}) = M.fromList $
-
-    -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster))
-
-    -- mod-button2, Raise the window to the top of the stack
+    [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
-
-    -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
-
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
 
