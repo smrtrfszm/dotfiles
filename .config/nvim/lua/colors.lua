fb032f7144eecadfc186ad935bc77874424eea3c
Szepesi Tibor <sztibi023@gmail.com>
2021-04-26 15:04:48 +0200

    add colors.lua

---
+++ .config/nvim/lua/colors.lua
@@ -0,0 +1,19 @@
+-- base16 default dark
+return {
+  black   = '#181818',
+  gray1   = '#282828',
+  gray2   = '#383838',
+  gray3   = '#585858',
+  gray4   = '#b8b8b8',
+  gray5   = '#d8d8d8',
+  gray6   = '#e8e8e8',
+  white   = '#f8f8f8',
+  red     = '#ab4642',
+  orange  = '#dc9656',
+  yellow  = '#f7ca88',
+  green   = '#a1b56c',
+  cyan    = '#86c1b9',
+  blue    = '#7cafc2',
+  magenta = '#ba8baf',
+  brown   = '#a16946',
+}
