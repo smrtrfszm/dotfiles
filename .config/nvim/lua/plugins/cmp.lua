eb52b802167b89f86b49734aa2d319c69cc0476c
Szepesi Tibor <sztibi023@gmail.com>
2022-02-11 00:29:48 +0100

    change autocomplete keybinds

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -5,7 +5,7 @@ cmp.setup({
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(5),
     ['<C-u>'] = cmp.mapping.scroll_docs(-5),
-    ['<CR>'] = cmp.mapping(
+    ['<Tab>'] = cmp.mapping(
       cmp.mapping.confirm(
         {
           behaviour = cmp.ConfirmBehavior.Insert,
@@ -14,14 +14,14 @@ cmp.setup({
         { 'i', 'c' }
       )
     ),
-    ['<Tab>'] = function(fallback)
+    ['<C-n>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       else
         fallback()
       end
     end,
-    ['<S-Tab>'] = function(fallback)
+    ['<C-p>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       else
