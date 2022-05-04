c13b23ae3711374b126cce44033333145e9a1917
Szepesi Tibor <sztibi023@gmail.com>
2022-05-04 19:25:08 +0200

    move python historyfile out from home folder

---
+++ .config/python/startup.py
@@ -0,0 +1,20 @@
+import os
+import atexit
+import readline
+
+if 'XDG_STATE_HOME' in os.environ:
+    history = os.path.join(os.path.expanduser(os.environ['XDG_STATE_HOME']), 'python_history')
+else:
+    history = os.path.join(os.path.expanduser('~'), '.local', 'state', 'python_history')
+
+history = os.path.abspath(history)
+history_dir, _ = os.path.split(history)
+os.makedirs(history_dir, exist_ok=True)
+
+try:
+    readline.read_history_file(history)
+    readline.set_history_length(1000)
+except FileNotFoundError:
+    pass
+
+atexit.register(readline.write_history_file, history)
--- .zshenv
+++ .zshenv
@@ -20,6 +20,7 @@ export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 export XMONAD_DATA_DIR="$XDG_DATA_HOME/xmonad"
 export XMONAD_CONFIG_DIR="$XDG_CONFIG_HOME/xmonad"
 export XMONAD_CACHE_DIR="$XDG_CACHE_HOME/xmonad"
+export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/startup.py"
 
 path+=("$HOME/scripts")
 path+=("$HOME/.local/bin")
