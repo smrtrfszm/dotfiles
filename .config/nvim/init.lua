e4c1d0662795cb48ea49202d61506e7c8e353651
Szepesi Tibor <sztibi023@gmail.com>
2022-11-21 01:47:16 +0100

    refactor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,15 @@
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local ensure_packer = function ()
+  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({
-    'git',
-    'clone',
-    '--depth', '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path
-  })
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system {'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path}
+    vim.cmd 'packadd packer.nvim'
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 require('settings')
 require('keys')
