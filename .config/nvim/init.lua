dea5419140e7bd4b46dfe088c6d1e529858c7c34
Szepesi Tibor <sztibi023@gmail.com>
2022-11-21 01:25:09 +0100

    use mason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,10 +54,6 @@ packer.startup(function(use)
     end,
   }
 
-  use {
-    'neovim/nvim-lspconfig',
-    config = function ()
-      require('plugins.lspconfig')
     end,
   }
 
@@ -156,10 +152,31 @@ packer.startup(function(use)
 
 
   use {
-    'williamboman/nvim-lsp-installer',
-    requires = 'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    requires = {
+      'williamboman/mason-lspconfig.nvim',
+      'neovim/nvim-lspconfig',
+    },
     config = function ()
-      require('plugins.lspinstall')
+      require('mason').setup()
+      require('mason-lspconfig').setup {}
+
+      require('mason-lspconfig').setup_handlers {
+        function (server_name)
+          require('lspconfig')[server_name].setup {}
+        end,
+        sumneko_lua = function ()
+          require('lspconfig').sumneko_lua.setup {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { 'vim' },
+                },
+              },
+            },
+          }
+        end,
+      }
     end,
   }
 
