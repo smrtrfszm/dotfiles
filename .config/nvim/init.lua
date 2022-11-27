4f619fe8c669ec837e9f2765671587e4bec8df18
Szepesi Tibor <sztibi023@gmail.com>
2022-11-28 00:38:07 +0100

    set capabilities

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -155,14 +155,19 @@ packer.startup(function(use)
     requires = {
       'williamboman/mason-lspconfig.nvim',
       'neovim/nvim-lspconfig',
+      'hrsh7th/cmp-nvim-lsp',
     },
     config = function ()
       require('mason').setup()
       require('mason-lspconfig').setup {}
 
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       require('mason-lspconfig').setup_handlers {
         function (server_name)
-          require('lspconfig')[server_name].setup {}
+          require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+          }
         end,
         sumneko_lua = function ()
           require('lspconfig').sumneko_lua.setup {
