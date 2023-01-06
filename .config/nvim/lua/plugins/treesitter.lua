0004f958849937fe48c468dcf3062082b7cd00e8
Szepesi Tibor <sztibi023@gmail.com>
2023-01-06 18:13:05 +0100

    disable additional regex

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = 'all',
   highlight = {
     enable = true,
+    additional_vim_regex_highlighting = false,
   },
   indent = {
     enable = true,
