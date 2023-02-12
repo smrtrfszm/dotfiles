821701daf76fe2ac530e766e2aa33ed3d45c967a
Szepesi Tibor <sztibi023@gmail.com>
2023-02-12 17:44:47 +0100

    add code actions keybind

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -15,6 +15,7 @@ vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
 vim.keymap.set('n', '<c-f>', function () require('telescope.builtin').find_files() end)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename)
+vim.keymap.set('n', 'ca', vim.lsp.buf.code_action)
 vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>')
 
 vim.keymap.set('n', '<leader>y', '"+y')
