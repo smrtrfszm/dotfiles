fb39a93cc8c81a0868d217c303d437a3f9082b4a
Szepesi Tibor <sztibi023@gmail.com>
2023-04-14 00:53:21 +0200

    remove editorconfig plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,8 +33,6 @@ packer.startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'lewis6991/impatient.nvim'
 
-  use 'editorconfig/editorconfig-vim'
-
   use {
     'nvim-treesitter/nvim-treesitter',
     requires = 'p00f/nvim-ts-rainbow',
