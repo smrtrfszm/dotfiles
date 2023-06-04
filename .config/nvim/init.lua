7d257bae4f35d666c6f8f372bd7785399370a76d
Szepesi Tibor <sztibi023@gmail.com>
2023-06-04 15:02:35 +0200

    enable blueprint_ls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -191,12 +191,14 @@ packer.startup(function(use)
         dapui.close()
       end
 
-
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require('lspconfig').blueprint_ls.setup {
+        capabilities = capabilities,
+      }
 
       require('mason-lspconfig').setup_handlers {
         function (server_name)
-
           require('lspconfig')[server_name].setup {
             capabilities = capabilities,
           }
