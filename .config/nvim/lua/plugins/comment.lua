876142e797b890b3dcf7fd2e5e311e2b6bd79c8f
Szepesi Tibor <sztibi023@gmail.com>
2024-03-10 18:52:56 +0100

    use builtin ts_context_commentstring integration function

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -15,22 +15,8 @@ return {
         basic = true,
         extra = true,
       },
-      pre_hook = function(ctx)
-        if vim.bo.filetype == 'svelte' or vim.bo.filetype == 'typescriptreact' then
-            local U = require('Comment.utils')
-            local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-            local location = nil
-            if ctx.ctype == U.ctype.block then
-                location = require('ts_context_commentstring.utils').get_cursor_location()
-            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-                location = require('ts_context_commentstring.utils').get_visual_start_location()
-            end
-
-            return require('ts_context_commentstring.internal').calculate_commentstring({
-                key = type,
-                location = location,
-            })
-        end
+      pre_hook = function (ctx)
+        require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()(ctx)
       end,
       post_hook = nil,
     },
