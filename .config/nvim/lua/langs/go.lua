2b6cb3f12e3d9466a7de102169d5d448a7b892ab
Szepesi Tibor <sztibi023@gmail.com>
2024-04-21 13:51:05 +0200

    add proto settings

--- .config/nvim/lua/langs/go.lua
+++ .config/nvim/lua/langs/go.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "go", "gomod", "gosum", "gowork" })
+      vim.list_extend(opts.ensure_installed, { "go", "gomod", "gosum", "gowork", "proto" })
     end
   },
 
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -62,7 +62,7 @@ local indentations = {
   toml            = { width=4, style='space', ruler=121 },
   blueprint       = { width=2, style='space', ruler=121 },
   markdown        = { width=4, style='space', ruler=80 },
-  proto           = { width=4, style='space', ruler=121 },
+  proto           = { width=2, style='space', ruler=121 },
   sql             = { width=2, style='space', ruler=121 },
 }
 
