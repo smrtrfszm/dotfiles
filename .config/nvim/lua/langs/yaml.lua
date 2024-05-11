bcb0abb6a49c762914cce36fe7909915c91c45f2
Szepesi Tibor <sztibi023@gmail.com>
2024-05-11 20:26:51 +0200

    update kubernetes file filter

--- .config/nvim/lua/langs/yaml.lua
+++ .config/nvim/lua/langs/yaml.lua
@@ -21,7 +21,7 @@ return {
           settings = {
             yaml = {
               schemas = {
-                kubernetes = "*.yaml",
+                kubernetes = "{k8s,kubernetes}/*.{yml,yaml}",
                 ["https://json.schemastore.org/github-workflow"] = ".github/workflows/*",
                 ["https://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
                 ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
