3647266d7c8dca0ebe015d6028f9b90890842c85
Szepesi Tibor <sztibi023@gmail.com>
2024-05-11 17:39:08 +0200

    update compose.yaml filter

--- .config/nvim/lua/langs/yaml.lua
+++ .config/nvim/lua/langs/yaml.lua
@@ -26,7 +26,7 @@ return {
                 ["https://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
                 ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
                 ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
-                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*(docker-)?compose*.{yml,yaml}",
               }
             }
           }
