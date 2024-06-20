cfdd8aa82358f0c40178b07def4da257bf83f29c
Szepesi Tibor <sztibi023@gmail.com>
2024-06-20 21:36:11 +0200

    add terraform config

---
+++ .config/nvim/lua/langs/terraform.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { 'terraform' })
+    end
+  },
+
+  {
+    "nvim-lspconfig",
+    opts = {
+      servers = {
+        terraformls = {}
+      }
+    }
+  }
+
+}
