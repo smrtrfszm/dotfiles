4c7bf3d9c0fc4f7171097dfbbdb8b52d0a36ba27
Szepesi Tibor <smrtrfszm@proton.me>
2024-06-29 09:10:56 +0200

    auto install diff parser

--- .config/nvim/lua/langs/git.lua
+++ .config/nvim/lua/langs/git.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-treesitter',
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { 'gitcommit' })
+      vim.list_extend(opts.ensure_installed, { 'gitcommit', 'diff' })
     end,
   },
 }
