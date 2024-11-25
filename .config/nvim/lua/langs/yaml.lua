e9c2695d370c3dc090cb847aa53ee86067b3e005
Szepesi Tibor <smrtrfszm@proton.me>
2024-11-25 19:09:58 +0100

    disable autoFetch in lazygit

--- .config/nvim/lua/langs/yaml.lua
+++ .config/nvim/lua/langs/yaml.lua
@@ -27,6 +27,7 @@ return {
                 ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
                 ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '*gitlab-ci*.{yml,yaml}',
                 ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*(docker-)?compose*.{yml,yaml}',
+                ['https://raw.githubusercontent.com/jesseduffield/lazygit/refs/heads/master/schema/config.json'] = '*lazygit/config.{yml,yaml}',
               }
             }
           }
