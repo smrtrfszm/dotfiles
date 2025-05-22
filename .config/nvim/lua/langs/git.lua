e5760e9003e7ec32dcd3ed5e25545fef883db16a
Szepesi Tibor <smrtrfszm@proton.me>
2025-05-23 01:42:31 +0200

    add more git highlights

--- .config/nvim/lua/langs/git.lua
+++ .config/nvim/lua/langs/git.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-treesitter',
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { 'gitcommit', 'diff' })
+      vim.list_extend(opts.ensure_installed, { 'diff', 'gitcommit', 'gitattributes', 'gitignore', 'git_config', 'git_rebase' })
     end,
   },
 }
