559e9277976712befea4d79938e1875bbb8bbbef
Szepesi Tibor <smrtrfszm@proton.me>
2025-06-07 03:05:39 +0200

    unnest dependency

--- .config/nvim/lua/langs/rust.lua
+++ .config/nvim/lua/langs/rust.lua
@@ -1,13 +1,15 @@
 return {
   {
+    'Saecki/crates.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    event = { 'BufRead Cargo.toml' },
+    config = true,
+  },
+
+  {
     'nvim-cmp',
     dependencies = {
-      {
-        'Saecki/crates.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        event = { 'BufRead Cargo.toml' },
-        config = true,
-      },
+      'Saecki/crates.nvim',
     },
     opts = function(_, opts)
       local cmp = require('cmp')
