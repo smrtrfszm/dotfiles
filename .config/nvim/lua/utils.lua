638551cdfd163a0bf232f7220c67cd588f03b2ef
Szepesi Tibor <smrtrfszm@proton.me>
2025-06-07 03:29:43 +0200

    move lazy install to utils

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -15,4 +15,27 @@ function M.set_opts(options)
   end
 end
 
+function M.ensure_lazy()
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+
+    if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+      }, true, {})
+
+      vim.fn.getchar()
+
+      os.exit(1)
+    end
+  end
+
+  vim.opt.rtp:prepend(lazypath)
+end
+
 return M
